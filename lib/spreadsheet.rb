62545faf4fa174fe29c203d65692caa83e26448e
Camille Lenormand <camille.lenormand02@gmail.com>
2023-04-26 14:20:47 +0200

    add gitignore

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -1,7 +1,10 @@
 # lib/spreadsheet.rb
 
-require_relative 'lib/mayors.rb'
+require "google_drive"
+require_relative "lib/mayors.rb"
+
+session = GoogleDrive::Session.from_config("config.json")
 
 def save_as_spreadsheet(file)
-  
+
 end
